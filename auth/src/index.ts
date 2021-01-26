0594a306e852706fe2766c1f6f1943805eb577dd
Brian Porter <porterdbrian@gmail.com>
2021-01-26 17:50:59 -0600

    Added mongodb pod and service to connect with mongoose

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import 'express-async-errors';
 import { json } from 'body-parser';
+import mongoose from 'mongoose';
 
 import { currentUserRouter } from './routes/currentuser';
 import { signinRouter } from './routes/signin';
@@ -24,6 +25,22 @@ app.all('*', async (req, res) => {
 
 app.use(errorHandler);
 
-app.listen(3000, () => {
-    console.log('Listening on port 3000!');
-});
+const startDb = async () => {
+    try {
+        await mongoose.connect('mongodb://auth-mongo-srv:27017/auth', {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true
+        });
+
+        console.log('CONNECTED TO MONGODB')
+    } catch (error) {
+        console.error(error);
+    }
+ 
+    app.listen(3000, () => {
+        console.log('Listening on port 3000!');
+    });
+}
+
+startDb();
