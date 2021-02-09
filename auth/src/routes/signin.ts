1e2e041c2ed39251ac36859d1af3ba470c4eb194
Brian Porter <porterdbrian@gmail.com>
2021-02-09 16:01:10 -0600

    Signin user logic

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,6 +1,10 @@
 import express, { Request, Response} from 'express';
 import { body } from 'express-validator';
+import jwt from 'jsonwebtoken';
 import {validateRequest} from '../middlewares/validate-request';
+import { User } from '../models/user';
+import { BadRequestError } from '../errors/bad-request-error';
+import {Password} from '../utils/password';
 
 const router = express.Router();
 
@@ -16,8 +20,35 @@ router.post('/api/users/signin',
     ]
     , 
     validateRequest,
-    (req: Request, res: Response) => {
+    async (req: Request, res: Response) => {
+        const { email, password } = req.body;
 
+        const existingUser = await User.findOne({email: email});
+        if (!existingUser) {
+            throw new BadRequestError('Invalid credentials.');
+        }
+
+        const passwordsMatch = await Password.compare(
+            existingUser.password, 
+            password
+        );
+        if (!passwordsMatch) {
+            throw new BadRequestError('Invalid credentials.');
+        }
+
+        //  Generate jwt
+        const userJwt = jwt.sign({
+            id: existingUser.id,
+            email: existingUser.email
+        }, process.env.JWT_KEY!
+        );
+
+        // Store in session object
+        req.session = {
+            jwt: userJwt
+        };
+
+        res.status(200).send(existingUser);
     }
 );
 
